88971b380157e18c50892ebf93835628442bd722
Maliksalmon2008 <83311416+Maliksalmon2008@users.noreply.github.com>
2021-07-22 14:46:13 -0700

    Add files via upload

--- main.js
+++ main.js
@@ -1,50 +1,55 @@
-var mouseevent="empty";
-var lastpositionX="";
-var lastpositionY="";
+var mouseEvent="empty";
+var lastpositionX;
+var lastpositionY;
 var color="purple";
 var width="10";
 var canvas=document.getElementById("myCanvas");
 var ctx=canvas.getContext("2d");
 
 
+
 canvas.addEventListener("mousedown",my_mousedown);
 function my_mousedown(e){
 color=document.getElementById("color").value;
 width=document.getElementById("width").value;
-mouseevent="mouseDown";
+mouseEvent="mouseDown";
 
 }
 canvas.addEventListener("mouseup",my_mouseup);
 function my_mouseup(e){
-mouseevent="MouseUp";
+mouseEvent="mouseUp";
 }
 
 canvas.addEventListener("mouseleave",my_mouseleave);
 function my_mouseleave(e){
-mouseevent="mouseleave";
+mouseEvent="mouseleave";
 }
 
 canvas.addEventListener("mousemove",my_mousemove);
 function my_mousemove(e){
-mouseevent="mousemove";
 
-var currentpositionofX=e.clientX - canvas.offsetLeft;
-var currentpositionofY=e.clientY - canvas.offsetTop;
-if(mouseevent=="mouseDown"){
+ currentpositionofX=e.clientX - canvas.offsetLeft;
+ currentpositionofY=e.clientY - canvas.offsetTop;
+if(mouseEvent=="mouseDown"){
  ctx.beginPath();
  ctx.strokeStyle=color;
  ctx.lineWidth=width;
  ctx.moveTo(lastpositionX,lastpositionY);
- lastpositionX=currentpositionofX;
- lastpositionY=currentpositionofY;
+
  console.log("last position of X and Y cooradnites")
  console.log("X="+lastpositionX+"Y"+lastpositionY);
- ctx.lineTo(currentpositionofX,currentpositionofY)
+ ctx.lineTo(currentpositionofX,currentpositionofY);
  ctx.stroke();
  console.log("Current position of X and Y cooradnites")
  console.log("X="+currentpositionX+"Y"+currentpositionY);
-
+}
+lastpositionX=currentpositionofX;
+ lastpositionY=currentpositionofY;
 
 }
-
+function clear_area(){
+    ctx.clearRect(0,0, ctx.canvas.width,ctx.canvas.height);
 }
+ 
+
+
